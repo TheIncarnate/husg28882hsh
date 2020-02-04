80a459e299bbb303175253546001826722677c6e
ItsAviral <60238390+Userbotbro@users.noreply.github.com>
2020-02-04 19:55:07 +0530

    Update antispam.py

--- haruka/modules/antispam.py
+++ haruka/modules/antispam.py
@@ -96,7 +96,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     ok123 = mention_html(user_chat.id, user_chat.first_name)
 
 
-    text12 = f"Breaking his ribs... {ok123}."
+    text12 = f"Breaking his ribs..."
     update.effective_message.reply_text(text12, parse_mode=ParseMode.HTML)
 
     banner = update.effective_user  # type: Optional[User]
