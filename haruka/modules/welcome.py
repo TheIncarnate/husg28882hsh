2504acbcc8113449f473b01bf28d5fa5d8c6f0cc
ItsAviral <60238390+Userbotbro@users.noreply.github.com>
2020-02-23 01:22:20 +0530

    Update welcome.py

--- haruka/modules/welcome.py
+++ haruka/modules/welcome.py
@@ -135,12 +135,12 @@ def new_member(bot: Bot, update: Update):
                     buttons = sql.get_welc_buttons(chat.id)
                     keyb = build_keyboard(buttons)
                 else:
-                    res = sql.DEFAULT_WELCOME.format(first=first_name)
+                  res = random.choice(sql.DEFAULT_WELCOME_MESSAGES).format(first=first_name)
                     keyb = []
 
                 keyboard = InlineKeyboardMarkup(keyb)
 
-                sent = send(update, res, keyboard,
+               sent = send(update, res, keyboard, random.choice(sql.DEFAULT_WELCOME_MESSAGES).format(first=escape_markdown(first_name)))
                             sql.DEFAULT_WELCOME.format(first=first_name))  # type: Optional[Message]
 
                 #Federations Ban
