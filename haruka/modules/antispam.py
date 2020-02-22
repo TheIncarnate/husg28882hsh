faea290b05a9b80f00f193d3a515d3c37e822ddc
ItsAviral <60238390+Userbotbro@users.noreply.github.com>
2020-02-22 12:37:47 +0530

    Update antispam.py

--- haruka/modules/antispam.py
+++ haruka/modules/antispam.py
@@ -96,7 +96,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     ok123 = mention_html(user_chat.id, user_chat.first_name)
 
 
-    text12 = f"Breaking his ribs..."
+    text12 = f"On it!"
     update.effective_message.reply_text(text12, parse_mode=ParseMode.HTML)
 
     banner = update.effective_user  # type: Optional[User]
@@ -138,7 +138,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                    "{} has been successfully gbanned!".format(mention_html(user_chat.id, user_chat.first_name)),
                    html=True)
-    text13 = f"Successfully gbanned {ok123} 🙂 'He Deserve This'."
+    text13 = f""gban complete! (User banned in {} chats)".format(gbanned_chats)"
     update.effective_message.reply_text(text13, parse_mode=ParseMode.HTML)
 
 
